d9c5449da0d9e31114985a861433289dabb637d9
kanz-cerk <kanzaniyah@yahoo.co.id>
2025-05-30 15:45:46 +0700

    Add files via upload

---
+++ AzimutKiblatAppGradleReady/app/src/main/java/com/example/azimutkiblatfixed/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.azimutkiblatfixed;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setAllowContentAccess(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/azimut.html");
+    }
+}
